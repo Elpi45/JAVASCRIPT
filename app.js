1f785c1337acb5f642dc03e5735b61fe8d1dc197
perez leonardo <leoperez2340@gmail.com>
2022-08-02 23:40:08 -0300

    Primer Entrega

---
+++ app.js
@@ -0,0 +1,8 @@
+//console.log("conectado")
+
+ let productos = prompt("ingresa el producto o Listo para terminar")
+
+ while(productos != "Listo"){
+          console.log("producto ingresado nombre " + productos)
+         productos = prompt("ingresa el producto de tu lista o Listo para terminar")
+}
